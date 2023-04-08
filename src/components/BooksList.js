85bde75960633e05e434e1039e8027a859bd1a2e
tofa93 <tofig.valiyev1993@gmail.com>
2023-04-08 23:02:02 +0200

    added new page

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -5,8 +5,8 @@ const BooksList = ({ subjectBooks, modalBtn }) => {
                 return (
                     <div className="book-preview" key={book.key}>
                         <button className="book-btn" onClick={() => modalBtn(book)} title={book.title}>
-                            <div className="book-poster">
-                                <img src={`https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`} alt="book-poster" className="book-poster-img" />
+                            <div className="book-cover">
+                                <img src={`https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`} alt="book-cover" className="book-cover-img" />
                             </div>
                             <div className="book-info">
                                 <div className="book-tile">{book.title}</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,14 @@ import BooksList from "./BooksList";
 import BookModal from "./BookModal";
 
 const Home = () => {
-    // const { data: books, isPending, error } = useFetch('https://api.npoint.io/387ac13a260ef4783834');
-    // const { data: books, isPending, error } = useFetch('https://openlibrary.org/subjects/horror.json');
     const [showModal, setShowModal] = useState(false);
     const [selectedBook, setSelectedBook] = useState(null);
-    const [activeGenre, setActiveGenre] = useState('');
     const [storeHeader, setStoreHeader] = useState('Horror');
     const { data: books, isPending, error } = useFetch(`https://openlibrary.org/subjects/${storeHeader.toLowerCase()}.json`);
     const [subjectBooks, setSubjectBooks] = useState(null);
     const [searchValue, setSearchValue] = useState('');
-
-    // const [newUrl, setNewUrl] = useState('');
-    // const { data: srchBook } = useFetch(newUrl);
-
-    const handleGenreClick = (genre) => {
-
-        setActiveGenre(genre);
-        setStoreHeader(genre);
-    }
+    const [newUrl, setNewUrl] = useState('');
+    const { data: srchBook } = useFetch(newUrl);
 
     function modalBtn(book) {
         setSelectedBook(book);
@@ -31,9 +21,9 @@ const Home = () => {
         setShowModal(false);
         setSelectedBook(null);
     }
-    // function searchBooks() {
-    //     setNewUrl(`https://openlibrary.org/subjects/horror.json`);
-    // }
+    function searchBooks() {
+        setNewUrl(`https://openlibrary.org/subjects/horror.json`);
+    }
     useEffect(() => {
         if (books) {
             setSubjectBooks(books.works);
@@ -48,116 +38,18 @@ const Home = () => {
                 <h1>Welcome to ArcBoox</h1>
             </div>
             <div className="library-area">
-                <div className="sidenav">
-                    <div className="search-box">
-                        {/* <input type="text" className="search-field" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-                        <button className="search-btn" onClick={searchBooks}>
-                            <span className="material-symbols-outlined">search</span>
-                        </button> */}
-                        {/* <form className="search-form">
-                            
-                        </form> */}
-                    </div>
-                    <div className="genres-header">
-                        <span className="genres-header-title">Pick your favorite genre !</span>
-                    </div>
-                    <div className="genre-tab">
-                        <button
-                            className={`genre-btn ${activeGenre === 'Fiction' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Fiction')}
-                        >
-                            Fiction
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Horror' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Horror')}
-                        >
-                            Horror
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Mistery' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Mistery')}
-                        >
-                            Mistery
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'History' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('History')}
-                        >
-                            History
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Fantasy' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Fantasy')}
-                        >
-                            Fantasy
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Design' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Design')}
-                        >
-                            Design
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Romance' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Romance')}
-                        >
-                            Romance
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Detective' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Detective')}
-                        >
-                            Detective
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Thriller' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Thriller')}
-                        >
-                            Thriller
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Mathematics' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Mathematics')}
-                        >
-                            Mathematics
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Magic' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Magic')}
-                        >
-                            Magic
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Programming' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Programming')}
-                        >
-                            Programming
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Poetry' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Poetry')}
-                        >
-                            Poetry
-                        </button>
-                        <button
-                            className={`genre-btn ${activeGenre === 'Humor' ? 'active' : ''}`}
-                            onClick={() => handleGenreClick('Humor')}
-                        >
-                            Humor
-                        </button>
-                    </div>
-                </div>
                 <div className="book-store">
-                    <div className="book-store-header">
-                        <span className="genres-header-title">{storeHeader}</span>
-                    </div>
-                    {isPending && <div>Loading</div>}
-                    {subjectBooks && <BooksList subjectBooks={subjectBooks} modalBtn={modalBtn} />}
+                    <div className="books-block">
+                        <div className="book-block-header">
+                            <span className="genres-header-title">{storeHeader}</span>
+                        </div>
+                        {isPending && <div>Loading</div>}
+                        {subjectBooks && <BooksList subjectBooks={subjectBooks} modalBtn={modalBtn} />}
 
-                    {/* {isPending && <div>Loading</div>}
+                        {/* {isPending && <div>Loading</div>}
                     {searchedBooks && <BooksList books={searchedBooks} modalBtn={modalBtn} />}
                     {!searchedBooks && books && <BooksList books={books} modalBtn={modalBtn} />} */}
+                    </div>
                 </div>
             </div>
         </div>
