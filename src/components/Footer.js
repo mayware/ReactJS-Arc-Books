085d448f09a453683fad791689ee10056c78b8bb
tofa93 <tofig.valiyev1993@gmail.com>
2023-04-26 19:21:20 +0200

    Updated footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
+import '../styles/footer.css'
+import { NavLink } from 'react-router-dom';
+
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>Footer</h2>
+
+            <div className='footer-left'>
+                <NavLink to="/" exact className="footer-brand-name-link">
+                    ArcBooks
+                </NavLink>
+            </div>
+            <div className="footer-middle">
+                <span className="copyright-title">
+                    &copy; {new Date().getFullYear()} Tofig. All rights reserved.
+                </span>
+            </div>
+            <div className="footer-right">
+                <a href="https://www.facebook.com/" className="soc-link"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.facebook.com/" className="soc-link"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.facebook.com/" className="soc-link"><i class="fa-brands fa-pinterest"></i></a>
+                <a href="https://www.facebook.com/" className="soc-link"><i class="fa-brands fa-twitter"></i></a>
+            </div>
         </div>
     );
 }
