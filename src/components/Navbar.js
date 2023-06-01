8a7fafa6b8f8cb7749a3c4a25a662ae9edc968db
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-01 18:07:33 +0200

    added dropdown features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { NavLink, Link } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { useHistory } from 'react-router-dom';
 import CartContext from "../context/CartContext";
 import '../styles/navbar.css'
@@ -9,12 +9,32 @@ const Navbar = ({ changeCurrentPage }) => {
     const { cartItems } = useContext(CartContext);
     const [searchTerm, setSearchTerm] = useState('');
     const [counter, setCounter] = useState(null);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
     const history = useHistory();
 
     function navbarSearch() {
         history.push(`/search?q=${searchTerm}`);
         setSearchTerm('');
         changeCurrentPage();
+    }
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        window.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            window.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
+    function dropdownBtn() {
+        setShowDropdown(!showDropdown);
     }
 
     useEffect(() => {
@@ -53,28 +73,29 @@ const Navbar = ({ changeCurrentPage }) => {
                     </div>}
                     <span className="navbar-link-span">Bookshelf</span>
                 </NavLink>
-                <NavLink to="/login" exact className="nav-link login-link">
-                    <span class="material-symbols-outlined navbar-link-icon">account_circle</span>
-                    <span className="navbar-link-span">Account</span>
-                </NavLink>
-                <div className="nav-dropdown">
-                    <div className="dropdown-box">
-                        <ul className="dropdown-menu">
-                            <li className="menu-item">
-                                <a href="#" className="drop-item">
-                                    <span className="drop-item-text">Log in</span>
-                                    <span class="material-symbols-outlined">login</span>
-                                </a>
-                            </li>
-                            <li className="menu-item">
-                                <a href="#" className="drop-item">
-                                    <span className="drop-item-text">Log out</span>
-                                    <span class="material-symbols-outlined">logout</span>
-                                </a>
-                            </li>
-                        </ul>
+                <button className="navbar-dropdown-btn" onClick={dropdownBtn}>
+                    <span className="material-symbols-outlined">account_circle</span>
+                </button>
+                {showDropdown && (
+                    <div className="nav-dropdown" ref={dropdownRef}>
+                        <div className="dropdown-box">
+                            <ul className="dropdown-menu">
+                                <li className="menu-item">
+                                    <NavLink to="/login" exact className="login-link">
+                                        <span className="drop-item-text">Log in</span>
+                                        <span className="material-symbols-outlined">login</span>
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink to="/login" exact className="login-link">
+                                        <span className="drop-item-text">Log out</span>
+                                        <span className="material-symbols-outlined">logout</span>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </header>
     );
