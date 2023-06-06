ba1e9616afacedbaf857c56a7f92c19bb61024b1
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-06 23:57:57 +0200

    Fixed bugs, added new features

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,35 @@
+import { useRef } from 'react';
+
 import '../styles/login.css'
 
 const Login = () => {
+
+    const email = useRef('');
+    const password = useRef('');
+
+    function inputCatcher(e) {
+        email.current = e.target.value;
+        password.current = e.target.value;
+    }
+
+    function loginSubmit(e) {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+    }
     return (
         <div className="content">
-            <h1>Login</h1>
-            <div className="login-box">
-                <form className="login-form">
-                    <input type="email" name="email" className="login-email-field" />
-                    <input type="password" name="password" className="login-password-field" />
-                    <input type="submit" value="Submit" />
-                </form>
+            <div className="login-content">
+                <h1>Login</h1>
+                <div className="login-box">
+                    <form className="login-form" onSubmit={loginSubmit}>
+                        <input type="email" ref={email} onChange={inputCatcher} name="email" className="login-email-field" />
+                        <input type="password" ref={password} onChange={inputCatcher} name="password" className="login-password-field" />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
+
         </div>
     );
 }
