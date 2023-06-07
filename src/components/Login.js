66b19e3e006e90008aa993e0c8dbdb3c644b4101
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-07 02:23:17 +0200

    added new features, requires more fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/login.css'
 
@@ -9,6 +10,7 @@ const Login = () => {
 
     function loginSubmit(e) {
         e.preventDefault();
+        // set up the validation
         email.current.value = '';
         password.current.value = '';
     }
@@ -18,10 +20,28 @@ const Login = () => {
                 <div className="login-box">
                     <div className='login-form-title'>Login</div>
                     <form className="login-form" onSubmit={loginSubmit}>
-                        <input type="email" ref={email} onChange={(e) => (e.target.value)} name="email" className="login-email-field" placeholder='Email' />
-                        <input type="password" ref={password} onChange={(e) => (e.target.value)} name="password" className="login-password-field" placeholder='Password' />
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            ref={email}
+                            onChange={(e) => (e.target.value)}
+                            name="email"
+                            className="login-email-field"
+                            placeholder='Email' />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            ref={password}
+                            onChange={(e) => (e.target.value)}
+                            name="password"
+                            className="login-password-field"
+                            placeholder='Password' />
                         <input type="submit" value="Log in" className='login-submit-btn' />
                     </form>
+                    <div className="sign-option">
+                        <span className="signup-option-title">Don't have an account?</span>
+                        <Link to="/signup" className='sign-up-link'>Sign up</Link>
+                    </div>
                 </div>
             </div>
         </div>
