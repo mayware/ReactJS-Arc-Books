19c6ca937b5e666b1abad179be10d7732e0f851f
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-18 13:15:17 +0200

    fixed few bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useState, useEffect, useContext, useRef } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useHistory } from 'react-router-dom';
 import CartContext from "../context/CartContext";
 import '../styles/navbar.css'
@@ -9,46 +9,13 @@ const Navbar = ({ changeCurrentPage }) => {
     const { cartItems } = useContext(CartContext);
     const [searchTerm, setSearchTerm] = useState('');
     const [counter, setCounter] = useState(null);
-    const [showDropdown, setShowDropdown] = useState(false);
-    const dropdownRef = useRef(null);
-    const dropdownBtnRef = useRef(null);
-    const searchBoxRef = useRef();
     const history = useHistory();
-
     const disabledBtn = searchTerm.length < 4;
 
     function navbarSearch() {
         history.push(`/search?q=${searchTerm}`);
         setSearchTerm('');
         changeCurrentPage();
-    }
-
-    useEffect(() => {
-        searchBoxRef.current.focus();
-        const handleOutsideClick = (event) => {
-            if (
-                dropdownRef.current &&
-                !dropdownRef.current.contains(event.target) &&
-                !dropdownBtnRef.current.contains(event.target)
-            ) {
-                setShowDropdown(false);
-            }
-        };
-
-        window.addEventListener('mouseup', handleOutsideClick);
-        return () => {
-            window.removeEventListener('mouseup', handleOutsideClick);
-        };
-    }, []);
-
-    function dropdownBtn(event) {
-        event.stopPropagation();
-        setShowDropdown(!showDropdown);
-    }
-    const handleLoginLinkClick = () => {
-        setTimeout(() => {
-            setShowDropdown(false);
-        }, 100);
     }
 
     function onChanger(e) {
@@ -69,7 +36,7 @@ const Navbar = ({ changeCurrentPage }) => {
                 </div>
                 <div className="header-left-links">
                     <div className="search-box">
-                        <input type="text" value={searchTerm} onChange={onChanger} ref={searchBoxRef} className="search-field" placeholder="Search.." />
+                        <input type="text" value={searchTerm} onChange={onChanger} className="search-field" placeholder="Search.." />
                         <button disabled={disabledBtn} className="search-btn" onClick={() => { navbarSearch() }}>
                             <span className="material-symbols-outlined">search</span>
                         </button>
